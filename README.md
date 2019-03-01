1cad22ea476965b0d8d47feed7dd91b893215c32
Lucas Martins <lukas932@hotmail.com>
2019-03-01 01:55:16 -0300

    Adicionado novos plugins e configurações ao webpack

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
 # webpack-config
 
-Configuração do webpack + babel
+## Configuração do webpack + babel
+
+- UglifyJs não suporta a sintaxe do es2015 em diante
+
+## Variáveis de ambiente nodejs
+
+Para garantir que as variáveis de ambiente do nodejs funcionem em qualquer plataforma (Windows, MacOs, Linux...) instalar a library:
+
+```
+npm i cross-env@5.0.1 -D
+```
+
+```
+// package.json
+"scripts": {
+    "build-prod": "cross-env NODE_ENV=production"
+}
+```
+
+## Importando Bootstrap como um módulo
+
+Ao realizarmos esse tipo de import (sem './' ou '../'), o webpack entende que ele deverá buscar o arquivo dentro da pasta node_modules.
+
+```
+// app.js
+import 'bootstrap/dist/css/bootstrap.css';
+```
+
+O webpack não entende arquivos css como módulos, portanto, precisaremos instalar novos loaders:
+
+```
+npm i css-loader@0.28.4 style-loader@0.18.2 -D
+```
+
+Loaders para tratar as fontes do Bootstrap:
+
+```
+npm i url-loader@0.5.9 file-loader@0.11.2 -D
+```
+
+## Importando um css qualquer
+
+```
+// app.js
+import '../css/style.css';
+```
+
+## FOUC (Flash of Unstyled Content)
+
+O webpack adicionou todos os arquivos css e js no mesmo bundle.js e via javascript está adicionando na tag <style> os conteúdos css.
+
+Para gerarmos um arquivo css separado do js precisaremos do plugin extract-text-webpack-plugin.
+
+```
+npm i extract-text-webpack-plugin@3.0.0 -D
+```
+
+## Importando scripts
+
+```
+import 'bootstrap/js/modal.js'
+```
+
+Problema: o jquery não está no escopo para os módulos da aplição, sendo assim a modal retorna erro: jQuery is not defined.
+
+Para isso utilizaremos o webpack.ProvidePlugin.
+
+## Removendo os imports de css e js do index.html
+
+```
+npm i html-webpack-plugin@2.29.0 -D
+```
+
+Esse plugin gera um html (através de um html template) importando todos os arquivos que foram gerados após o build e adiciona na dist.
+
+## Code splitting e Lazy loading
+
+```
+const { LazyService } = await System.import('../services/LazyService);
+const service = new LazyService();
+```
+
+## Alterando o endereço da API no build de produção
+
+Através do plugin webpack.DefinePlugin você define o nome da variável que será procurada em todos os módulos da aplicação e ele fará a substituição do valor que foi atribuido a essa variável.
+
+```
+this.http.get(`${API_URL}/negociações/semana`)
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,99 @@
 // módulo do nodejs
 const path = require('path');
 
+// plugin para realizar o uglify do bundle
+const babiliPlugin = require('babili-webpack-plugin');
+
+// plugin para extrair os conteúdos css em um arquivo separado
+const extractTextPlugin = require('extract-text-webpack-plugin');
+
+// plugin para remover os imports de css e js do index.html
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+// webpack
+const webpack = require ('webpack');
+
+// array de plugins
+let plugins = [];
+
+plugins.push(new htmlWebpackPlugin({
+
+    // adiciona um hash ao final dos arquivos css e js para invalidar o cache
+    hash: true,
+
+    // configurações para minificar
+    minify: {
+        html5: true,
+        
+        // remove os espaços entre as tags
+        collapseWhiteSpace: true,
+
+        // remove os comentários
+        removeComments: true
+    },
+
+    // nome do arquivo que será gerado
+    filename: 'index.html',
+
+    // template que será utilizado para gerar o arquivo index.html
+    template: __dirname + '/main.html'
+}))
+
+// recebe como parâmetro o nome do arquivo que será gerado
+plugins.push(new extractTextPlugin('styles.css'))
+
+// Sepação do nosso código das bibliotecas 
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    
+    // identificador
+    name: 'vendor',
+
+    // bundle onde as bibliotecas ficarão
+    filename: 'vendor.bundle.js',
+}));
+
+// api de desenvolvimento
+let SERVICE_URL = JSON.stringify('http://api-develop.com');
+
+// process é a variável que dá acesso ao processo do nodejs
+if (process.env.NODE_ENV == 'production') {
+
+    // api de produção
+    SERVICE_URL = JSON.stringify('http://api-producao.com');
+
+    // otmização para a geração do bundle
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+
+    // uglify
+    plugins.push(new babiliPlugin());
+
+    // adicionando as variáveis '$' e 'jQuery' para serem acessadas por todos os módulos da aplicação
+    plugins.push(new webpack.ProvidePlugin({
+        '$': 'jquery/dist/jquery.js',
+        'jQuery': 'jquery/dist/jquery.js'
+    }))
+}
+
+// plugin para alterar o endereço da api de acordo com o ambiente
+plugins.push(new webpack.DefinePlugin({
+    
+    // recebe como parâmetro o nome da váriavel que será procurada na aplicação e o valor que será atribuído
+    API_URL: SERVICE_URL
+}));
+
 // módulo do nodejs
 module.exports = {
 
     // primeiro módulo a ser carregado na minha aplicação
-    entry: './app-src/app.js',
+    entry: {
+        
+        // módulo de entrada da aplição, a partir desse arquivo o webpack criará o arquivo bundle.js
+        app: './app-src/app.js',
+
+        // identificador declarado na configuração do CommonsChunkPlugin
+        // bibliotecas que serão adicionadas no arquivo vendor.bundle.js
+        vendor: ['jquery', 'bootstrap']
+    },
 
     output: {
 
@@ -13,6 +101,63 @@ module.exports = {
         filename: 'bundle.js',
 
         // caminho onde será gravado o bundle gerado
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+
+        // configuração para que o webpack-dev-server crie o bundle em memória, porém na path dist 
+        publicPath: 'dist'
+    },
+
+    module: {
+
+        // regras que serão aplicadas no módulo antes da geração do bundle
+        rules: [
+            {
+                // através de uma expressão regular, indicamos que para cada arquivo com a extensão .js o loader será aplicado
+                test: /\.js$/,
+
+                // arquivos que não quero que a regra seja aplicadas
+                exclude: /node_modules/,
+                
+                // loader que será executado, nesse caso, a transpilação do typescript
+                use: {
+                    loader: 'babel-loader'
+                }
+            }, 
+
+            {
+                // indicamos que para cada arquivo com a extensão .css o loader será aplicado
+                test: /\.css$/,
+                use: extractTextPlugin({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+
+                // css-loader tranformará o arquivo css em um objeto javascript (json)
+                // style-loader pegará esse objeto js (json) e tranformará o css em line para colocar no html
+                // o sinal ! representa que será executado um loader em seguida o outro. Sendo a execução da direita para a esquerda
+                // loader: 'style-loader!css-loader'
+            },
+
+            // regras para tratar as fontes do Bootstrap
+            { 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
+            }
+        ]
+    },
+
+    // plugins que serão executados após a geração do bundle
+    plugins
 }
